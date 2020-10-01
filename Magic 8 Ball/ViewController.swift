007a6b804cc8c1051d1a7f510ced7593cb033975
Paul Wang <5761415+paulwang007@users.noreply.github.com>
2020-10-01 00:58:47 -0700

    Dev (#1)
    * Initial commit
    
    * Add basic UI
    
    * last commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBOutlet weak var mainPageTitle: UILabel!
+    
+    @IBOutlet weak var magicBallImage: UIImageView!
+    
+    @IBAction func onPressAskButton(_ sender: UIButton) {
+        // ..< means inclusive.
+        magicBallImage.image = ballArray[Int.random(in: 0..<5)]
+    }
 }
-
