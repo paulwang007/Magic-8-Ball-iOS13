e2f0d4a6f4d3fcd40649ee2106e81262e9099af2
Paul Wang <5761415+paulwang007@users.noreply.github.com>
2020-10-01 00:22:45 -0700

    Initial commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,4 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
 }
-
